b7914ce65c95e417ff7c03f164b6a621dd68a7bd
Devon Stewart <blast@hardchee.se>
2015-04-18 15:48:37 -0700

    Adding decoder for IHDR data

--- PngParser.hs
+++ PngParser.hs
@@ -11,10 +11,23 @@ import Data.Word (Word8)
 
 import qualified Data.ByteString as B
 import Data.Attoparsec.ByteString
+import qualified Data.Attoparsec.ByteString as AP
+
+data ChunkData = ChunkIHDRData {
+    width :: Int,
+    height :: Int,
+    bitDepth :: Int,
+    colorType :: Int,
+    compressionMethod :: Int,
+    filterMethod :: Int,
+    interlaceMethod :: Int
+} deriving Show
 
 data PngChunk = Chunk {
     length :: Int,
-    type' :: B.ByteString
+    type' :: B.ByteString,
+    data' :: ChunkData,
+    crc :: Int
 } deriving Show
 
 data PngStructure = Png {
@@ -22,16 +35,27 @@ data PngStructure = Png {
     chunks :: [PngChunk]
 } deriving Show
 
+int8 :: Parser Int
+int8 = fromIntegral <$> anyWord8
+
 int32 :: Parser Int
 int32 = (foldl (\a x -> (shift 8 a) .|. (fromIntegral x)) 0) <$> count 4 anyWord8
 
 pngHeader = string $ B.pack [0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a]
 
 pngChunk :: Parser PngChunk
-pngChunk = Chunk <$> int32 <*> (B.pack <$> count 4 anyWord8)
+pngChunk = do
+    len <- int32
+    typ <- AP.take 4
+    (bits, dat) <- match $ decodeData typ
+    crc <- int32
+    return $ Chunk len typ dat crc
 
 pngFile :: Parser PngStructure
 pngFile = Png <$> pngHeader <*> count 1 pngChunk
+
+decodeData :: B.ByteString -> Parser ChunkData
+decodeData "IHDR" = ChunkIHDRData <$> int32 <*> int32 <*> int8 <*> int8 <*> int8 <*> int8 <*> int8
 
 insertIndex :: Int -> B.ByteString -> B.ByteString -> B.ByteString
 insertIndex idx toInsert file = B.concat [B.take idx file, toInsert, B.drop idx file]
