d9face35b0b579aa2ed3aba5f4494694de0a676c
Devon Stewart <blast@hardchee.se>
2015-04-18 23:35:45 -0700

    Moving data types to PNG module

--- Png.hs
+++ Png.hs
@@ -4,10 +4,46 @@ import Control.Applicative ((<$>), (*>), (<*>))
 import Control.Monad ((>>), (>>=))
 
 import Data.Bits (shift, xor, (.|.), (.&.))
-
+import qualified Data.ByteString as B
 import Data.Word (Word8, Word32)
 
 import Debug.Trace (trace)
+
+data ChunkData = ChunkIHDRData {
+    width :: Word32,
+    height :: Word32,
+    bitDepth :: Word8,
+    colorType :: Word8,
+    compressionMethod :: Word8,
+    filterMethod :: Word8,
+    interlaceMethod :: Word8
+} | ChunksBITData {
+    red :: Word8,
+    green :: Word8,
+    blue :: Word8
+} | ChunkpHYs {
+    ppux :: Word32,
+    ppuy :: Word32,
+    unitSpecifier :: Word8
+} | ChunktEXt {
+    keyword :: B.ByteString,
+    text :: B.ByteString
+} | ChunkIDAT {
+    bits :: B.ByteString
+} | ChunkIEND {
+} deriving Show
+
+data PngChunk = Chunk {
+    length :: Word32,
+    type' :: B.ByteString,
+    data' :: ChunkData,
+    crc :: Word32
+} deriving Show
+
+data PngStructure = Png {
+    header :: B.ByteString,
+    chunks :: [PngChunk]
+} deriving Show
 
 buildCrc :: Word32 -> Word32
 buildCrc c = foldr acc c [0..7]
--- PngParser.hs
+++ PngParser.hs
@@ -17,42 +17,6 @@ import qualified Data.Attoparsec.ByteString as AP
 
 import Png
 
-data ChunkData = ChunkIHDRData {
-    width :: Word32,
-    height :: Word32,
-    bitDepth :: Word8,
-    colorType :: Word8,
-    compressionMethod :: Word8,
-    filterMethod :: Word8,
-    interlaceMethod :: Word8
-} | ChunksBITData {
-    red :: Word8,
-    green :: Word8,
-    blue :: Word8
-} | ChunkpHYs {
-    ppux :: Word32,
-    ppuy :: Word32,
-    unitSpecifier :: Word8
-} | ChunktEXt {
-    keyword :: B.ByteString,
-    text :: B.ByteString
-} | ChunkIDAT {
-    bits :: B.ByteString
-} | ChunkIEND {
-} deriving Show
-
-data PngChunk = Chunk {
-    length :: Word32,
-    type' :: B.ByteString,
-    data' :: ChunkData,
-    crc :: Word32
-} deriving Show
-
-data PngStructure = Png {
-    header :: B.ByteString,
-    chunks :: [PngChunk]
-} deriving Show
-
 int8 :: Parser Word8
 int8 = fromIntegral <$> anyWord8
 
