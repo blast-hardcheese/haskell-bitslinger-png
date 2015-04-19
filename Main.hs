42b0b11f0c51bb65f0edb63ef8ff656af96197e0
Devon Stewart <blast@hardchee.se>
2015-04-19 00:09:25 -0700

    Round-trip the PNG

--- Main.hs
+++ Main.hs
@@ -4,9 +4,11 @@ import qualified Data.ByteString as B
 import Control.Applicative ((<$>))
 
 import PngParser (parse, fix)
+import Png
 
 main :: IO ()
 main = do
     fixed <- (B.readFile "corrupt_735acee15fa4f3be8ecd0c6bcf294fd4.png" >>= fix)
-    let res = parse fixed
-    putStrLn $ show res
+    let (Right res) = parse fixed
+    putStrLn "Seems to have parsed"
+    B.writeFile "roundtrip.png" $ encodePng res
